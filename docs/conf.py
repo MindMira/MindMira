dc4da949f34ee2bc9f363a3ea4f7b74c64e4f9f8
rudals6151 <rudals6151@naver.com>
2024-11-27 18:30:38 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,23 @@
-cd docs
-sphinx-quickstart
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'MindMira'
+copyright = '2024, MindMira Contributors'
+author = 'MindMira Team'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
